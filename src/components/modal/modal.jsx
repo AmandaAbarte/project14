0674cd6f6d5da34324e59dea4980e05a670055db
AmandaAbarte <amanda.abarte@gmail.com>
2023-03-13 17:57:14 +0100

    add navigation to employee table from modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setModal } from "../../context/modalSlice.slice";
+import { useNavigate } from "react-router-dom";
 
 export default function Modal() {
+  let navigate = useNavigate();
   const dispatch = useDispatch();
   const showModal = useSelector((state) => state.modal.showModal);
-
+  const employees = () => {
+    navigate("/employees");
+    dispatch(setModal(false));
+  };
   const closeModal = () => {
     dispatch(setModal(false));
   };
@@ -13,9 +18,14 @@ export default function Modal() {
     showModal && (
       <div className="modal-container">
         <div className="modal-content">
-          <p>Employee saved!</p>
-          <p onClick={closeModal} className="close-modal">
-            X
+          <div className="modal-title">
+            <p>Employee saved!</p>
+            <p onClick={closeModal} className="close-modal">
+              X
+            </p>
+          </div>
+          <p onClick={employees} className="a">
+            See registered employees
           </p>
         </div>
       </div>
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import employeesReducer from "./employeeSlice.slice";
 import modalSlice from "./modalSlice.slice";
+
 export const store = configureStore({
   reducer: {
     employee: employeesReducer,
