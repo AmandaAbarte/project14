b9ae583bf41cd624a0a9b4a86e7ae06577d9c890
AmandaAbarte <amanda.abarte@gmail.com>
2023-03-15 13:29:54 +0100

    add date picker

--- src/components/employeeForm.jsx
+++ src/components/employeeForm.jsx
@@ -4,14 +4,15 @@ import { addEmployee } from "../context/employeeSlice.slice";
 import { setModal } from "../context/modalSlice.slice";
 import Dropdown from "./dropdown/dropdown";
 import Modal from "./modal/modal";
+import DatePicker from "react-date-picker";
 
 export default function CreateEmployee() {
   const dispatch = useDispatch();
   const dropdowns = useSelector((state) => state.dropdown.dropdowns);
+  const [dateofbirth, setdateofbirth] = useState(new Date());
   const [formData, setFormData] = useState({
     firstname: "",
     lastname: "",
-    dateofbirth: "",
     startdate: "",
     street: "",
     city: "",
@@ -31,8 +32,16 @@ export default function CreateEmployee() {
   //dispatch empoyee data to store on submit
   const saveEmployee = (e) => {
     e.preventDefault();
+    let dateDMY = `${dateofbirth.getDate()}-${
+      dateofbirth.getMonth() + 1
+    }-${dateofbirth.getFullYear()}`;
+    dispatch(
+      addEmployee({
+        ...formData,
 
-    dispatch(addEmployee(formData));
+        dateofbirth: dateDMY,
+      })
+    );
     dispatch(setModal(true));
   };
   const states = [
@@ -107,23 +116,56 @@ export default function CreateEmployee() {
       <form onSubmit={saveEmployee}>
         <h2>Create Employee</h2>
         <label htmlFor="firstname">First Name</label>
-        <input type="text" id="firstname" onChange={handleInput} required />
+        <input
+          type="text"
+          id="firstname"
+          onChange={handleInput}
+          required
+          className="input"
+        />
         <label htmlFor="lastname">Last Name</label>
-        <input type="text" id="lastname" onChange={handleInput} required />
+        <input
+          type="text"
+          id="lastname"
+          onChange={handleInput}
+          required
+          className="input"
+        />
 
         <label htmlFor="dateofbirth">Date of Birth</label>
-        <input id="dateofbirth" type="date" onChange={handleInput} required />
-
+        {/* <input id="dateofbirth" type="date" onChange={handleInput} required /> */}
+        <DatePicker
+          onChange={setdateofbirth}
+          value={dateofbirth}
+          format="dd-MM-y"
+        />
         <label htmlFor="startdate">Start Date</label>
-        <input id="startdate" type="date" onChange={handleInput} required />
+        <input
+          id="startdate"
+          type="date"
+          onChange={handleInput}
+          required
+          className="input"
+        />
         <fieldset className="address">
           <legend>Address</legend>
 
           <label htmlFor="street">Street</label>
-          <input id="street" type="text" onChange={handleInput} />
+          <input
+            id="street"
+            type="text"
+            onChange={handleInput}
+            className="input"
+          />
 
           <label htmlFor="city">City</label>
-          <input id="city" type="text" onChange={handleInput} required />
+          <input
+            id="city"
+            type="text"
+            onChange={handleInput}
+            required
+            className="input"
+          />
 
           <label htmlFor="state">State</label>
           <Dropdown
@@ -134,29 +176,24 @@ export default function CreateEmployee() {
           />
 
           <label htmlFor="zipcode">Zip Code</label>
-          <input id="zipcode" type="number" onChange={handleInput} required />
+          <input
+            id="zipcode"
+            type="number"
+            onChange={handleInput}
+            required
+            className="input"
+          />
         </fieldset>
         <label htmlFor="department">Department</label>
-        {/* <select
-          name="department"
-          id="department"
-          onChange={handleInput}
-          required
-        >
-          
-          <option>Sales</option>
-          <option>Marketing</option>
-          <option>Engineering</option>
-          <option>Human Resources</option>
-          <option>Legal</option>
-        </select> */}
         <Dropdown
           list={departments}
           name="department"
           id="department"
           handler={handleInput}
         />
-        <button type="submit">Save</button>
+        <button type="submit" className="button">
+          Save
+        </button>
       </form>
       <Modal />
     </>
