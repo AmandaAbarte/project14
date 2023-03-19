17ad36910686c5cad596099ba2d188b54128937c
AmandaAbarte <amanda.abarte@gmail.com>
2023-03-19 12:28:02 +0100

    better render of table

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -4,37 +4,51 @@ import "./table.css";
 export default function Table({ columns, data }) {
   const [search, setSearch] = useState("");
   const [filterData, setFilterData] = useState(data);
+  //define the order of data keys based on column title order
+  let sortOrder = [];
+  columns.map((column) => {
+    sortOrder.push(column.key);
+  });
 
-  // map columns and for each title
+  //loop over data and reorganise based on sortOrder, comparing the value of item.key
+  filterData.map((item) => {
+    const itemPositions = {};
+    for (const [index, key] of sortOrder.entries()) {
+      itemPositions[key] = index;
+    }
+    item.sort((a, b) => itemPositions[a.key] - itemPositions[b.key]);
+  });
+
+  //loop over column array and return div of column titles
   const titles = columns.map((columnItem) => {
-    //loop over data array
-    const rows = filterData.map((dataItem) => {
-      //loop over each object in array
-      const rowItem = dataItem.map((item) => {
-        //check if data key matches title key and display data under appropriate title
-        if (item.key === columnItem.key) {
-          return (
-            <div className="row-item" key={columnItem.key}>
-              <p>{item.title}</p>
-            </div>
-          );
-        }
-      });
-      return rowItem;
-    });
     return (
-      <div key={columnItem.key}>
-        <div className="column">
-          <h3 className="column-title">{columnItem.title}</h3>
-        </div>
-        {rows}
+      <div className="column" key={columnItem.key}>
+        <h3 className="column-title">{columnItem.title}</h3>
       </div>
     );
   });
 
+  //loop over data and return table rows
+  const rows = filterData.map((dataItem) => {
+    const rowItems = dataItem.map((item) => {
+      return (
+        <div className="column" key={item.title}>
+          <p>{item.title}</p>
+        </div>
+      );
+    });
+    return (
+      <div className="row" key={dataItem[0].title}>
+        {rowItems}
+      </div>
+    );
+  });
+
+  //on change of search state, loop over data
   useEffect(() => {
     let newdata = [];
     data.map((item) => {
+      //if search matches any data, push data item to newdata
       const test = item.map((subitem) => {
         let lowercaseSubItem = subitem.title.toLowerCase();
         return lowercaseSubItem.includes(search.toLowerCase());
@@ -43,6 +57,7 @@ export default function Table({ columns, data }) {
         newdata.push(item);
       }
     });
+    //set filteredData state with new filtred value
     setFilterData(newdata);
   }, [search]);
 
@@ -56,7 +71,7 @@ export default function Table({ columns, data }) {
       />
       <div className="table-container">
         <div className="table-header">{titles}</div>
-        {/* {rows} */}
+        {rows}
       </div>
     </>
   );
