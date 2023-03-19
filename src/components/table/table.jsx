4a4cfb8a58cef27415680717127dc87032569786
AmandaAbarte <amanda.abarte@gmail.com>
2023-03-19 11:47:32 +0100

    table search functionality

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./table.css";
 
 export default function Table({ columns, data }) {
+  const [search, setSearch] = useState("");
+  const [filterData, setFilterData] = useState(data);
+
   // map columns and for each title
   const titles = columns.map((columnItem) => {
     //loop over data array
-    const rows = data.map((dataItem) => {
+    const rows = filterData.map((dataItem) => {
       //loop over each object in array
       const rowItem = dataItem.map((item) => {
         //check if data key matches title key and display data under appropriate title
@@ -20,11 +23,41 @@ export default function Table({ columns, data }) {
       return rowItem;
     });
     return (
-      <div className="column" key={columnItem.key}>
-        <h3 className="column-title">{columnItem.title}</h3>
+      <div key={columnItem.key}>
+        <div className="column">
+          <h3 className="column-title">{columnItem.title}</h3>
+        </div>
         {rows}
       </div>
     );
   });
-  return <div className="table-container">{titles}</div>;
+
+  useEffect(() => {
+    let newdata = [];
+    data.map((item) => {
+      const test = item.map((subitem) => {
+        let lowercaseSubItem = subitem.title.toLowerCase();
+        return lowercaseSubItem.includes(search.toLowerCase());
+      });
+      if (test.includes(true)) {
+        newdata.push(item);
+      }
+    });
+    setFilterData(newdata);
+  }, [search]);
+
+  return (
+    <>
+      <input
+        type="text"
+        onInput={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <div className="table-container">
+        <div className="table-header">{titles}</div>
+        {/* {rows} */}
+      </div>
+    </>
+  );
 }
